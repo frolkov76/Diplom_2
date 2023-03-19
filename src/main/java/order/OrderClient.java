7faa9504099a2d4f56b6f7983f9951bd160f6b50
Aleksandr Frolkov <frolkov76@gmail.com>
2023-03-19 07:38:08 +0700

    second commit

--- src/main/java/order/OrderClient.java
+++ src/main/java/order/OrderClient.java
@@ -1,2 +1,52 @@
-package order;public class OrderClient {
+package order;
+
+import client.Client;
+import io.qameta.allure.Step;
+import io.restassured.response.ValidatableResponse;
+import static io.restassured.RestAssured.given;
+
+public class OrderClient extends Client  {
+
+    private static String ORDER_PATH = "/api/orders";
+    // private static String INGREDIENT_PATH = "/api/ingredients";
+
+    @Step("Создание заказа с отправкой токена")
+    public ValidatableResponse createOrderWithToken(String token, Ingredients ingredients) {
+        return given()
+                .spec(getSpec())
+                .auth().oauth2(token)
+                .body(ingredients)
+                .when()
+                .post(ORDER_PATH)
+                .then();
+    }
+
+    @Step("Создание заказа без отправки токена")
+    public ValidatableResponse createOrderWithoutToken(Ingredients ingredient) {
+        return given()
+                .spec(getSpec())
+                .body(ingredient)
+                .when()
+                .post(ORDER_PATH)
+                .then();
+    }
+
+    @Step("Получение заказов конкретного пользователя с отправкой токена")
+    public ValidatableResponse getOrdersWithToken(String token) {
+        return given()
+                .spec(getSpec())
+                .auth().oauth2(token)
+                .when()
+                .get(ORDER_PATH)
+                .then();
+    }
+
+    @Step("Получение заказов конкретного пользователя без отправки токена")
+    public ValidatableResponse getOrdersWithoutToken() {
+        return given()
+                .spec(getSpec())
+                .when()
+                .get(ORDER_PATH)
+                .then();
+    }
 }
--- src/test/java/ordertests/OrderCreateTest.java
+++ src/test/java/ordertests/OrderCreateTest.java
@@ -1,2 +1,70 @@
-package ordertests;public class OrderCreateTest {
+package ordertests;
+
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.response.ValidatableResponse;
+import order.Ingredients;
+import order.OrderClient;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import user.User;
+import user.UserClient;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+public class OrderCreateTest {
+
+    private User user;
+    private UserClient userClient;
+    private OrderClient orderClient;
+    private String accessToken;
+    private Ingredients ingredients;
+
+    @Before
+    public void setUp() {
+        user = User.getUser();
+        userClient = new UserClient();
+        orderClient = new OrderClient();
+        ValidatableResponse createUserResponse = userClient.createUser(user);
+        accessToken = createUserResponse.extract().path("accessToken");
+        ingredients = Ingredients.getRandomBurger();
+    }
+
+    @After
+    public void tearDown() {
+        if (accessToken != null) {
+            userClient.deleteUser(user, accessToken.substring(7));
+        }
+    }
+
+    @Test
+    @DisplayName("Создание заказа с авторизацией и ингредиентом")
+    public void createOrderWithAuthTest() {
+        ValidatableResponse orderResponse = orderClient.createOrderWithToken(accessToken.substring(7), Ingredients.getRandomBurger()).statusCode(200);
+        orderResponse.assertThat().body("order.number", notNullValue());
+        orderResponse.assertThat().body("success", equalTo(true));
+    }
+
+    @Test
+    @DisplayName("Создание заказа без авторизации")
+    public void createOrderWithoutAuthTest() {
+        ValidatableResponse orderResponse = orderClient.createOrderWithoutToken(Ingredients.getRandomBurger()).statusCode(200);
+        orderResponse.assertThat().body("order.number", notNullValue());
+        orderResponse.assertThat().body("success", equalTo(true));
+    }
+
+    @Test
+    @DisplayName("Создание заказа без ингредиента")
+    public void createOrderWithoutIngredientTest() {
+        ValidatableResponse orderResponse = orderClient.createOrderWithToken(accessToken.substring(7), Ingredients.getNullIngredients()).statusCode(400);
+        orderResponse.assertThat().body("success", equalTo(false));
+        orderResponse.assertThat().body("message", equalTo("Ingredient ids must be provided"));
+    }
+
+    @Test
+    @DisplayName("Создание заказа c неверным хэшем ингредиента")
+    public void createOrderWithIncorrectIngredientTest() {
+        ValidatableResponse orderIncorrectIngredientResponse = orderClient.createOrderWithToken(accessToken.substring(7), Ingredients.getIncorrectIngredients()).statusCode(500);
+    }
 }
